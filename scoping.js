77fef160eb43ea2d69572b12a785427866d8ab27
Sethuramalingam Duraisamy <59419619+Sethuramalingam-art@users.noreply.github.com>
2023-07-01 11:48:32 +0530

    Create scoping.js

---
+++ scoping.js
@@ -0,0 +1,99 @@
+/// var only an function scope so inside function you defined a var that we can't access outside but inside block scope when we initialize
+// a var we can't get var outside
+
+var x = 100;
+
+function d() {
+  console.log(x); //100
+}
+d();
+console.log(x); //100
+
+// --------------------------------------------------------------------------------------------
+var x = 100;
+if (true) {
+  var t = 100000000; // var is function scope so it is block scope so we can access outside scope
+}
+function d() {
+  console.log(t); //100000000
+  console.log(x); //100
+}
+d();
+console.log(t); //100000000
+console.log(x); //100
+
+// // --------------------------------------------------------------------------------------------
+var x = 100;
+if (true) {
+  var t = 100000000; // var is function scope so it is block scope so we can access outside scope
+}
+function d() {
+  t = 20000000000000; // because t is global scope
+  x = 1231212; // because var is only function scope so it defined in block so no issues
+  console.log(t); //20000000000000
+  console.log(x); //100
+}
+d();
+console.log(t); //20000000000000
+console.log(x); //100
+
+// // --------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------
+var x = 100;
+if (true) {
+  var t = 100000000; // var is function scope so it is block scope so we can access outside scope
+}
+function d() {
+  var funcscope = 23423423423;
+  t = 20000000000000; // because t is global scope
+  x = 1231212; // because var is only function scope so it defined in block so no issues
+  console.log(t); //20000000000000
+  console.log(x); //100
+}
+d();
+console.log(funcscope); //it is not defined because var is function scope error
+console.log(t); //20000000000000
+console.log(x); //100
+
+// --------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------
+var x = 100;
+if (true) {
+  var t = 100000000; // var is function scope so it is block scope so we can access outside scope
+}
+function d() {
+  //var funcscope = 23423423423;
+  var t = 20000000000000; // because t is global scope
+  x = 1231212; // because var is only function scope so it defined in block so no issues
+  console.log(t); //20000000000000
+  console.log(x); //100
+}
+d();
+//console.log(funcscope); //it is not defined because var is function scope error
+console.log(t); //20000000000000
+console.log(x); //100
+
+// --------------------------------------------------------------------------------------------
+
+//let, const it is block scope
+
+console.log(r); // undefined
+var r = 12121;
+
+// --------------------------------------------------------------------------------------------
+
+let y = 100000;
+
+if (true) {
+  console.log(u);
+  var u = 100045678;
+
+  console.log(y); //100000
+  function x() {
+    console.log(y); //100000
+  }
+  x();
+}
+
+console.log(y); //100000
